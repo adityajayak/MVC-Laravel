93391f2d550872cca79c65a741e7bb626c400e2d
Aditya Jaya Kusuma <66470549+adityajayak@users.noreply.github.com>
2020-07-01 11:57:49 +0700

    Final 2

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,12 +6,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {   
-   public function welcome (){
-        return view ('welcome');
+   public function form(){
+        return view ('Register');
    }
-   public function welcome_post (Request $request ){
-    $fnama = $request["fname"];
-    $lnama = $request["lname"];
-    return view ('welcome'); 
+   public function welcomea_post (Request $request ){
+    $fname = $request["fname"];
+    $lname = $request["lname"];
+    return view ('welcomea', ['fname' => $fname],  ['lname' => $lname]); 
     }
 }
